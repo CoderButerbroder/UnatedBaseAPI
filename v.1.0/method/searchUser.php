f41a9c9aa51b05a291239951c57086aabb5d8f09
Victor <62008876+Xokma@users.noreply.github.com>
2020-10-22 13:37:59 +0300

    примерно как то так

--- v.1.0/method/searchUser.php
+++ v.1.0/method/searchUser.php
@@ -28,22 +28,50 @@ if (!headers_sent())          { header('Content-type:application/json;charset=ut
 
 
 
-$key = 'asdasdadasdweqwrq';
+$key = 'eJzbqfSooYm8P51dZ6sWnw5Oz786FGxZ';
 $asdasd = 'asdasd';
 // например, с помощью openssl_random_pseudo_bytes
-$plaintext = "данные для шифрования";
-$cipher = "aes-128-gcm";
-if (in_array($cipher, openssl_get_cipher_methods()))
-{
-    $ivlen = openssl_cipher_iv_length($cipher);
-    $iv = openssl_random_pseudo_bytes($ivlen);
-    $ciphertext = openssl_encrypt($plaintext, $cipher, $key, $options=0, $iv, $asdasd);
-    // сохраняем $cipher, $iv и $tag для дальнейшей расшифровки
-    $original_plaintext = openssl_decrypt($ciphertext, $cipher, $key, $options=0, $iv, $asdasd);
-    echo $original_plaintext."\n";
+//$plaintext = "данные для шифрования";
+
+$obj_data = (object) [
+                        "user" => 'lw0E1vLiW7BC4Db1HOdDMIrqzX4Vn99H',
+                        "data_making" => date("Y-m-d H:i:s"),
+                        "live" => "259200" // 3 дня в секундах
+];
+
+$plaintext = json_encode($obj_data, JSON_UNESCAPED_UNICODE);
+
+if(json_last_error() != 0){
+  echo "error";
+  exit();
 }
 
+echo "\nОригинальный текст: ".$plaintext."\n";
+foreach ($obj_data as $key => $value) {
+  echo $key." = ".$value."\n";
+}
+echo "\n";
 
+
+$method = "AES-128-CBC";
+
+$ivlen = openssl_cipher_iv_length($method);
+$iv = openssl_random_pseudo_bytes($ivlen);
+
+$text_cript = openssl_encrypt($plaintext, $method, $key, $options=0, $iv);
+
+// сохраняем $cipher, $iv и $tag для дальнейшей расшифровки
+$text_encript = openssl_decrypt($text_cript, $method, $key, $options=0, $iv);
+//echo $text_cript."\n";
+
+echo "\n***************************************************************\n";
+echo "\nЗашифрованный текст:".$text_cript."\n";
+echo "\n***************************************************************\n";
+echo "\nРасшифрованный текст:".$text_encript."\n";
+
+foreach (json_decode($text_encript, false) as $key => $value) {
+  echo $key." = ".$value."\n";
+}
 
 // $massiv = explode('/', trim($_SERVER['REQUEST_URI'],'/'));
 //
