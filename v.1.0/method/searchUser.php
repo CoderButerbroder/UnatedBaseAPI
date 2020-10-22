73cfeaa7a372fe8dea7fd77344f311b21a6cb9f2
Dmitriy Ivanov <dimos-eskimos2014@yandex.ru>
2020-10-22 13:05:00 +0300

    asd

--- v.1.0/method/searchUser.php
+++ v.1.0/method/searchUser.php
@@ -9,10 +9,10 @@ if (!headers_sent())          { header('Content-type:application/json;charset=ut
 
 // проверка наличия токена авторизации пользователя
 
-if (!$token_user) {
-    if ($response_type == 'json') echo json_encode(array('error' => 'token not found')); exit;
-    if ($response_type == 'xml') echo '<response><error>token not found</error></response>'; exit;
-}
+// if (!$token_user) {
+//     if ($response_type == 'json') echo json_encode(array('error' => 'token not found')); exit;
+//     if ($response_type == 'xml') echo '<response><error>token not found</error></response>'; exit;
+// }
 
 
 // проверка токена на действительность (в базе данных выданных токенов)
@@ -26,12 +26,23 @@ if (!$token_user) {
 // Выдача результата пользователю
 // (php код тут)
 
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"\?\>";
-echo " <response> ";
-echo "<form>";
-echo "123";
-echo "</form>";
-echo "</response> ";
+
+
+$key = 'asdasdadasdweqwrq';
+$asdasd = 'asdasd';
+// например, с помощью openssl_random_pseudo_bytes
+$plaintext = "данные для шифрования";
+$cipher = "aes-128-gcm";
+if (in_array($cipher, openssl_get_cipher_methods()))
+{
+    $ivlen = openssl_cipher_iv_length($cipher);
+    $iv = openssl_random_pseudo_bytes($ivlen);
+    $ciphertext = openssl_encrypt($plaintext, $cipher, $key, $options=0, $iv, $asdasd);
+    // сохраняем $cipher, $iv и $tag для дальнейшей расшифровки
+    $original_plaintext = openssl_decrypt($ciphertext, $cipher, $key, $options=0, $iv, $asdasd);
+    echo $original_plaintext."\n";
+}
+
 
 
 // $massiv = explode('/', trim($_SERVER['REQUEST_URI'],'/'));
