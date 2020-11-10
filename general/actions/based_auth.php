7d1b95642e796e142c8808cc143f338bb3c8b4f8
Dmitriy Ivanov <dimos-eskimos2014@yandex.ru>
2020-11-10 11:21:23 +0300

    asdad

--- general/actions/based_auth.php
+++ general/actions/based_auth.php
@@ -10,13 +10,8 @@ $check_login = $settings->check_login_valid($login);
 
 if (json_decode($check_login)->response) {
       $check_auth = $settings->auth_user($login,$password,$session_id,$ip);
-      if (json_decode($check_auth)->response) {
-          header('Location: /profile');
-          exit;
-      } else {
-          header('Content-type:application/json;charset=utf-8');
-          echo $check_auth;
-      }
+      echo $check_auth;
+      exit;
 } else {
       header('Content-type:application/json;charset=utf-8');
       echo $check_login;
