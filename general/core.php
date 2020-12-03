95ef7e1d541c9a56d30c812e6d2ca57df0479d32
Xokma <62008876+Xokma@users.noreply.github.com>
2020-12-03 16:20:28 +0300

    telega bot

--- general/core.php
+++ general/core.php
@@ -2547,7 +2547,34 @@ class Settings {
 
 
 
+    public function telega_send($id, $message) {   //Задаём публичную функцию send для отправки сообщений
+        //Заполняем массив $data инфой, которую мы через api отправим до телеграмма
+        $data = array(
+            'chat_id'      => $id,
+            'text'     => $message,
+        );
+        //Получаем ответ через функцию отправки до апи, которую создадим ниже
+        $out = $this->telega_request('sendMessage', $data);
+        //И пусть функция вернёт ответ. Правда в данном примере мы это никак не будем использовать, пусть будет задаток на будущее
+        return $out;
+    }
 
+    public  function telega_request($method, $data = array()) {
+        $curl = curl_init(); //мутим курл-мурл в переменную. Для отправки предпочтительнее использовать курл, но можно и через file_get_contents если сервер не поддерживает
+        $token = $this->get_global_settings('telega_token');
+
+        curl_setopt($curl, CURLOPT_URL, 'https://api.telegram.org/bot' . $token .  '/' . $method);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST'); //Отправляем через POST
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data); //Сами данные отправляемые
+
+        $out = json_decode(curl_exec($curl), true); //Получаем результат выполнения, который сразу расшифровываем из JSON'a в массив для удобства
+
+        curl_close($curl); //Закрываем курл
+
+        return $out; //Отправляем ответ в виде массива
+    }
 
 
 
@@ -3121,7 +3148,5 @@ class Settings {
 
 
 }
-
-
 
 ?>
