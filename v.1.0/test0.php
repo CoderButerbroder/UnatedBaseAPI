92f023b20acdb0fd700ff3c15967b262ecd103c4
Xokma <62008876+Xokma@users.noreply.github.com>
2020-12-04 23:57:56 +0300

    magic

---
+++ v.1.0/test0.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Базовая разметка HTML</title>
+</head>
+<!-- jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<body>
+    <h1>Code Basics</h1>
+    <input type="checkbox" id="checkbox" checked>
+</body>
+
+<script type="text/javascript">
+var flag = true;
+  $(document).ready(function(){
+
+    let timerId = setTimeout(function tick() {
+
+      if ($('#checkbox').is(':checked') && flag) {
+        flag = false;
+        $.ajax({
+        	url: 'https://<?php echo $_SERVER["SERVER_NAME"];?>/admin/help/sinc_tboil_user.php',
+        	method: 'get',
+          async : false,
+          timeout: 0,
+        	success: function(data){
+        		flag = true;
+        	},
+          error: function(x, t, e){
+            if( t === 'timeout') {
+                 // Произошел тайм-аут
+                } else {
+                     flag = true;
+                }
+            },
+        });
+      }
+
+      timerId = setTimeout(tick, 2000); // (*)
+    }, 2000);
+
+  });
+</script>
+</html>
