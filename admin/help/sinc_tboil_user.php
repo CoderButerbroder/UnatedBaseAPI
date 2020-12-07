2477c5315f6decdf90c7cf9632ee4ef6c3e41b3e
CoderButerbroder <dimos-eskimos2014@yandex.ru>
2020-12-08 00:41:32 +0300

    asdasd

--- admin/help/sinc_tboil_user.php
+++ admin/help/sinc_tboil_user.php
@@ -1,10 +1,10 @@
 <?php
+error_reporting(0);
 // ini_set('error_reporting', E_ALL);
 // ini_set('display_errors', 1);
 // ini_set('display_startup_errors', 1);
 // session_start();
 include('/home/httpd/vhosts/api.kt-segment.ru/httpdocs/general/core2.php');
-
 
 ignore_user_abort(true);
 set_time_limit(0);
