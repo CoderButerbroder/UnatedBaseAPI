0e0ea4d87e3055cb03d9adad1cf47d9afc5e4e05
Xokma <62008876+Xokma@users.noreply.github.com>
2020-12-07 16:40:01 +0300

    опечатка

--- v.1.0/method/updNewTechServicesRating.php
+++ v.1.0/method/updNewTechServicesRating.php
@@ -10,13 +10,13 @@ if ($id_services_rating_on_referer && $id_service_on_referer && $id_comment && $
       require_once($_SERVER['DOCUMENT_ROOT'].'/general/core.php');
       $settings = new Settings;
       $check_valid_token = $settings->validate_token($token,$resource);
-                           $settings->recording_history($resource,'addNewTechServicesRating',$check_valid_token);
+                           $settings->recording_history($resource,'updNewTechServicesRating',$check_valid_token);
 
       if (json_decode($check_valid_token)->response) {
             $check_id_referer = $settings->get_data_referer($resource);
             if (json_decode($check_id_referer)->response) {
                 $response = $settings->tech_services_rating($id_services_rating_on_referer,$id_service_on_referer,$id_comment,$id_user_tboil,$rating,$date_update,json_decode($check_id_referer)->data->id);
-                            $settings->recording_history($resource,'addNewTechServicesRating',$response);
+                            $settings->recording_history($resource,'updNewTechServicesRating',$response);
                   echo $response;
             } else {
                   echo $check_id_referer;
