ea805852ea303eeeb78fe39f964af846721bd907
Xokma <62008876+Xokma@users.noreply.github.com>
2020-12-15 11:08:53 +0300

    addNewTicket

--- v.1.0/method/addNewTicket.php
+++ v.1.0/method/addNewTicket.php
@@ -1,49 +1,46 @@
 <?
-// Добавление мероприятия в единую базу данных
+// Добавление нового тикета
 //add_new_support_ticket($id_tboil, $name, $description, $status, $id_referer)
-//add_new_support_messages($id_support_ticket, $id_tboil, $message, $links_add_files, $id_referer, $type_user) {
+//add_new_support_messages($id_support_ticket, $id_tboil, $message, $links_add_files, $id_referer, $type_user)
 
 
+include($_SERVER['DOCUMENT_ROOT'].'/v.1.0/settings.php');
 
+if (!$token) {echo json_encode(array('response' => false, 'description' => 'Обязательно требуется токен'),JSON_UNESCAPED_UNICODE);exit;}
+if (!$resource) {echo json_encode(array('response' => false, 'description' => 'Обязательно требуется ресурс с которого идет запрос'),JSON_UNESCAPED_UNICODE);exit;}
 
-// $id_tboil, $name, $description, $status = 'open'
-//
-// $message, $links_add_files, $type_user) {
-//
-//
-//
-//
-//
-//
-//
-//
-// include($_SERVER['DOCUMENT_ROOT'].'/v.1.0/settings.php');
-//
-// if (!$token) {echo json_encode(array('response' => false, 'description' => 'Обязательно требуется токен'),JSON_UNESCAPED_UNICODE);exit;}
-// if (!$resource) {echo json_encode(array('response' => false, 'description' => 'Обязательно требуется ресурс с которого идет запрос'),JSON_UNESCAPED_UNICODE);exit;}
-//
-// if ($id_event_on_referer && isset($place) && isset($interest) && isset($type_event) && isset($name) && isset($description) && isset($organizer) && isset($status) && isset($start_datetime_event) && isset($end_datetime_event)) {
-//       require_once($_SERVER['DOCUMENT_ROOT'].'/general/core.php');
-//       $settings = new Settings;
-//       $check_valid_token = $settings->validate_token($token,$resource);
-//                            $settings->recording_history($resource,'addNewTicket',$check_valid_token);
-//
-//       if (json_decode($check_valid_token)->response) {
-//           $check_id_referer = $settings->get_data_referer($resource);
-//           if (json_decode($check_id_referer)->response) {
-//               $response = $settings->add_update_new_event($id_event_on_referer,$type_event,$name,$description,$organizer,$status,$start_datetime_event,$end_datetime_event,$place,$interest,json_decode($check_id_referer)->data->id);
-//                           $settings->recording_history($resource,'addNewTicket',$response);
-//                 echo $response;
-//           } else {
-//                 echo $check_id_referer;
-//           }
-//       } else {
-//               echo $check_valid_token;
-//       }
-// } else {
-//       echo json_encode(array('response' => false, 'description' => 'Не все обязательные поля были заполнены для добавления тикета в единую базу данных', 'data_referer' => $_POST),JSON_UNESCAPED_UNICODE);
-//       exit;
-// }
+if (isset($id_tboil) &&  isset($name) &&  isset($description) &&  isset($message) &&  isset($links_add_files) &&  isset($type_user) ) {
+      require_once($_SERVER['DOCUMENT_ROOT'].'/general/core.php');
+      $settings = new Settings;
+      $check_valid_token = $settings->validate_token($token,$resource);
+                           $settings->recording_history($resource,'addNewTicket',$check_valid_token);
+
+      if (json_decode($check_valid_token)->response) {
+          $check_id_referer = $settings->get_data_referer($resource);
+          $data_refer = json_decode($check_id_referer);
+          if ($data_refer->response) {
+            $status = 'open';
+              $response_ticket = $settings->add_new_support_ticket($id_tboil, $name, $description, $status, $data_refer->data->id);
+                          $settings->recording_history($resource,'addNewTicket',$response);
+              if(json_decode($response_ticket)->response){
+                $response_ticket_message = $settings->add_new_support_messages($id_support_ticket, $id_tboil, $message, $links_add_files, $data_refer->data->id, $type_user);
+                            $settings->recording_history($resource,'addNewTicketMessage',$response);
+                if(json_decode($response_ticket_message)->response && json_decode($response_ticket)->response){
+                  echo json_encode(array('response' => true, 'description' => 'Тикет успешно открыт', 'data' => $response_ticket),JSON_UNESCAPED_UNICODE);
+                } else {
+                  echo $response_ticket;
+                }
+              }
+          } else {
+                echo $check_id_referer;
+          }
+      } else {
+              echo $check_valid_token;
+      }
+} else {
+      echo json_encode(array('response' => false, 'description' => 'Не все обязательные поля были заполнены для добавления тикета в единую базу данных', 'data_referer' => $_POST),JSON_UNESCAPED_UNICODE);
+      exit;
+}
 
 
 ?>
