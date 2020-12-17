65a2ecff82d22143323c9aba2a159bed29f18014
Xokma <62008876+Xokma@users.noreply.github.com>
2020-12-17 13:40:45 +0300

    +jq

--- panel/settings/users.php
+++ panel/settings/users.php
@@ -236,7 +236,11 @@
 
 <script type="text/javascript">
   $(document).ready(function(){
-
+    $('.table').DataTable({
+          "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Russian.json"
+          }
+    });
   });
 </script>
 
