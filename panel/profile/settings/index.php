1a7ab45d02c3432e5dac2966f42595ad15598a3a
Xokma <62008876+Xokma@users.noreply.github.com>
2020-12-18 17:31:31 +0300

    +

--- panel/profile/settings/index.php
+++ panel/profile/settings/index.php
@@ -19,7 +19,7 @@
               <div class="card-body">
                 <h5 class="card-title">Привяжите свои аккаунты социальных сетей прямо сейчас и авторизуйтесь через них без ввода логина и пароля</h5>
                 <h6 class="card-subtitle mb-2 text-muted"><script src="//ulogin.ru/js/ulogin.js"></script><div id="uLogin_fab20c8b" data-uloginid="fab20c8b"></div></h6>
-                  <div class="row" id="social_user_link">
+                  <div class="row mt-3" id="social_user_link">
                 </div>
               </div>
             </div>
