b0ded93f392217c2d83dd69d6a51d3d6278eb036
CoderButerbroder <dimos-eskimos2014@yandex.ru>
2020-12-18 19:54:12 +0300

    остановка на создании прав ролей

--- general/settings/default_rules.php
+++ general/settings/default_rules.php
@@ -1,22 +1,47 @@
 {
-"rules":
-    {
-     "view":
-          {
-            "all":false
-          },
-     "add":
-          {
-            "all":false
-          },
-     "edit":
-          {
-            "all":false
-          },
-
-     "delete":
-          {
-            "all":false
-          }
+  "sistem":{
+            "settings":{
+                "name":"settings",
+                "description":"Доступ к настройкам системы",
+                "value":true
+            },
+            "load":{
+                "name":"load",
+                "description":"Доступ к нагрузке системы",
+                "value":true
+            },
+            "loging":{
+                "name":"loging",
+                "description":"Доступ к логированию и ошибкам системы системы",
+                "value":true
+            },
+            "history":{
+                "name":"loging",
+                "description":"Доступ к истории системы",
+                "value":true
+            }
+  },
+  "users":{
+            "view_all_users":{
+                "name":"view_all_users",
+                "description":"Доступ к просмотру всех пользователей системы",
+                "value":true
+            },
+            "add_new_role":{
+                "name":"add_new_role",
+                "description":"Доступ к добавлению новых ролей",
+                "value":true
+            },
+            "view_all_role":{
+                "name":"view_all_role",
+                "description":"Доступ к просмотру всех ролей",
+                "value":true
+            },
+            "delete_role":{
+                "name":"delete_role",
+                "description":"Доступ к удалению ролей",
+                "value":true
+            }
     }
+
 }
--- panel/settings/view_rules.php
+++ panel/settings/view_rules.php
@@ -11,16 +11,42 @@ $data_role_json = $settings->get_role_data_name($name_role);
 
 $data_roles = json_decode($data_role_json);
 
-$alias_role = (isset($data_roles->data->alias)) ? $data_roles->data->alias : 'Ошибка';
+$alias_role = (isset($data_roles->data->alias)) ? $data_roles->data->alias : 'Ошибк';
 
+$data_json = json_decode('{
+"sistem":{
+          "settings":{
+              "name":"settings",
+              "description":"Доступ к настройкам системы",
+              "value":true
+          },
+          "load":{
+              "name":"load",
+              "description":"Доступ к нагрузке системы",
+              "value":true
+          },
+          "loging":{
+              "name":"loging",
+              "description":"Доступ к логированию и ошибкам системы системы",
+              "value":true
+          },
+          "history":{
+              "name":"loging",
+              "description":"Доступ к истории системы",
+              "value":true
+          }
+}
+}
+');
 
+var_dump($data_json);
 ?>
 
 <nav class="page-breadcrumb">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="#">Настройки</a></li>
     <li class="breadcrumb-item"><a href="/panel/settings/roles">Роли и права</a></li>
-    <li class="breadcrumb-item active" aria-current="page">Права роли <?php echo $alias_role;?></li>
+    <li class="breadcrumb-item active" aria-current="page">Права роли <?php echo $alias_role;?>а</li>
   </ol>
 </nav>
 
@@ -28,49 +54,57 @@ $alias_role = (isset($data_roles->data->alias)) ? $data_roles->data->alias : 'О
   <div class="row">
         <div class="col-md-12 stretch-card">
                   <div id="accordion" class="accordion" role="tablist" style="width: 100%">
-                                <div class="card">
+                              <div class="card">
                                   <div class="card-header" role="tab" id="heading1">
                                     <h6 class="mb-0">
-                                      <a data-toggle="collapse" href="#collapse1" aria-expanded="true" aria-controls="collapse1">
-                                        Настройки системы
+                                      <a data-toggle="collapse" href="#collapse1" aria-expanded="true" aria-controls="collapse1" style="color: #fff;">
+                                        <div class="row">
+                                          <div class="col-6 mt-2 pl-3" style="color: #000;">
+                                            Настройки системы
+                                          </div>
+                                          <div class="col-6 text-right">
+                                            <button type="button" class="btn btn-sm btn-success">Сохранить</button>
+                                          </div>
+                                        </div>
+
                                       </a>
                                     </h6>
                                   </div>
-                                  <div id="collapse1" class="collapse show" role="tabpanel" aria-labelledby="heading1" data-parent="#accordion">
+                                  <div id="collapse1" class="collapse" role="tabpanel" aria-labelledby="heading1" data-parent="#accordion">
                                     <div class="card-body">
-                                      <div class="row">
-                                          <div class="col-md-8">
-
+                                          <div class="alert alert-primary" role="alert">
+                                          Пожалуйста будьте осторожны при назначении прав в даном разделе настроек. Каждый пользователь получивший доступ к настройкам системы получает полный контроль над всем. Если вы не уверены в своих действиях, оставьте данный раздел пустым
                                           </div>
-                                          <div class="col-md-2">
-
-                                          </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-
-
-                                <div class="card">
-                                  <div class="card-header" role="tab" id="headingTwo">
-                                    <h6 class="mb-0">
-                                      <a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                        Настройки пользователей
-                                      </a>
-                                    </h6>
-                                  </div>
-                                  <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
-                                    <div class="card-body">
+                                          <?php foreach ($data_json->sistem as $key => $value) { ?>
+                                              <div class="row border-bottom">
+                                                <div class="col-sm-11 mt-2">
+                                                  <?php echo $value->description;?>
+                                                </div>
+                                                <div class="col-sm-auto form-check">
+                                                      <label class="form-check-label">
+                                                        <input type="checkbox" name="<?php $value->name?>" <?php if ($value->value) {?>checked<?}?>class="form-check-input">
+                                                      </label>
+                                                </div>
+                                              </div>
+                                          <? }?>
 
                                     </div>
                                   </div>
-                                </div>
+                              </div>
+
 
                                 <div class="card">
                                   <div class="card-header" role="tab" id="headingUsers">
                                     <h6 class="mb-0">
-                                      <a class="collapsed" data-toggle="collapse" href="#collapseUsers" aria-expanded="false" aria-controls="collapseUsers">
-                                        Настройки пользователей
+                                      <a class="collapsed" data-toggle="collapse" href="#collapseUsers" aria-expanded="false" aria-controls="collapseUsers" style="color: #fff;">
+                                        <div class="row">
+                                          <div class="col-6 mt-2 pl-3" style="color: #000;">
+                                            Настройки пользователей
+                                          </div>
+                                          <div class="col-6 text-right">
+                                            <button type="button" class="btn btn-sm btn-success">Сохранить</button>
+                                          </div>
+                                        </div>
                                       </a>
                                     </h6>
                                   </div>
@@ -84,8 +118,15 @@ $alias_role = (isset($data_roles->data->alias)) ? $data_roles->data->alias : 'О
                                 <div class="card">
                                   <div class="card-header" role="tab" id="headingDataCompany">
                                     <h6 class="mb-0">
-                                      <a class="collapsed" data-toggle="collapse" href="#collapseDataCompany" aria-expanded="false" aria-controls="collapseDataCompany">
-                                        Данные компаний
+                                      <a class="collapsed" data-toggle="collapse" href="#collapseDataCompany" aria-expanded="false" aria-controls="collapseDataCompany" style="color: #fff;">
+                                        <div class="row">
+                                          <div class="col-6 mt-2 pl-3" style="color: #000;">
+                                            Данные компаний
+                                          </div>
+                                          <div class="col-6 text-right">
+                                            <button type="button" class="btn btn-sm btn-success">Сохранить</button>
+                                          </div>
+                                        </div>
                                       </a>
                                     </h6>
                                   </div>
@@ -99,8 +140,15 @@ $alias_role = (isset($data_roles->data->alias)) ? $data_roles->data->alias : 'О
                                 <div class="card">
                                   <div class="card-header" role="tab" id="headingDataUser">
                                     <h6 class="mb-0">
-                                      <a class="collapsed" data-toggle="collapse" href="#collapseDataUser" aria-expanded="false" aria-controls="collapseDataUser">
-                                        Данные пользователей
+                                      <a class="collapsed" data-toggle="collapse" href="#collapseDataUser" aria-expanded="false" aria-controls="collapseDataUser" style="color: #fff;">
+                                        <div class="row">
+                                          <div class="col-6 mt-2 pl-3" style="color: #000;">
+                                            Данные пользователей
+                                          </div>
+                                          <div class="col-6 text-right">
+                                            <button type="button" class="btn btn-sm btn-success">Сохранить</button>
+                                          </div>
+                                        </div>
                                       </a>
                                     </h6>
                                   </div>
@@ -115,12 +163,112 @@ $alias_role = (isset($data_roles->data->alias)) ? $data_roles->data->alias : 'О
                                 <div class="card">
                                   <div class="card-header" role="tab" id="headingDataEvents">
                                     <h6 class="mb-0">
-                                      <a class="collapsed" data-toggle="collapse" href="#collapseDataEvents" aria-expanded="false" aria-controls="collapseDataEvents">
-                                        Данные мероприятий
+                                      <a class="collapsed" data-toggle="collapse" href="#collapseDataEvents" aria-expanded="false" aria-controls="collapseDataEvents" style="color: #fff;">
+                                        <div class="row">
+                                          <div class="col-6 mt-2 pl-3" style="color: #000;">
+                                            Данные мероприятий
+                                          </div>
+                                          <div class="col-6 text-right">
+                                            <button type="button" class="btn btn-sm btn-success">Сохранить</button>
+                                          </div>
+                                        </div>
                                       </a>
                                     </h6>
                                   </div>
                                   <div id="collapseDataEvents" class="collapse" role="tabpanel" aria-labelledby="headingDataEvents" data-parent="#accordion">
+                                    <div class="card-body">
+
+                                    </div>
+                                  </div>
+                                </div>
+
+
+                                <div class="card">
+                                  <div class="card-header" role="tab" id="headingDataReports">
+                                    <h6 class="mb-0">
+                                      <a class="collapsed" data-toggle="collapse" href="#collapseDataReports" aria-expanded="false" aria-controls="collapseDataReports" style="color: #fff;">
+                                        <div class="row">
+                                          <div class="col-6 mt-2 pl-3" style="color: #000;">
+                                            Данные отчетов
+                                          </div>
+                                          <div class="col-6 text-right">
+                                            <button type="button" class="btn btn-sm btn-success">Сохранить</button>
+                                          </div>
+                                        </div>
+                                      </a>
+                                    </h6>
+                                  </div>
+                                  <div id="collapseDataReports" class="collapse" role="tabpanel" aria-labelledby="headingDataReports" data-parent="#accordion">
+                                    <div class="card-body">
+
+                                    </div>
+                                  </div>
+                                </div>
+
+
+
+                                <div class="card">
+                                  <div class="card-header" role="tab" id="headingSupports">
+                                    <h6 class="mb-0">
+                                      <a class="collapsed" data-toggle="collapse" href="#collapseSupports" aria-expanded="false" aria-controls="collapseSupports" style="color: #fff;">
+                                        <div class="row">
+                                          <div class="col-6 mt-2 pl-3" style="color: #000;">
+                                            Тех.поддержка
+                                          </div>
+                                          <div class="col-6 text-right">
+                                            <button type="button" class="btn btn-sm btn-success">Сохранить</button>
+                                          </div>
+                                        </div>
+                                      </a>
+                                    </h6>
+                                  </div>
+                                  <div id="collapseSupports" class="collapse" role="tabpanel" aria-labelledby="headingSupports" data-parent="#accordion">
+                                    <div class="card-body">
+
+                                    </div>
+                                  </div>
+                                </div>
+
+
+                                <div class="card">
+                                  <div class="card-header" role="tab" id="headingDashbord">
+                                    <h6 class="mb-0">
+                                      <a class="collapsed" data-toggle="collapse" href="#collapseDashbord" aria-expanded="false" aria-controls="collapseDashbord" style="color: #fff;">
+                                        <div class="row">
+                                          <div class="col-6 mt-2 pl-3" style="color: #000;">
+                                            Дашборд
+                                          </div>
+                                          <div class="col-6 text-right">
+                                            <button type="button" class="btn btn-sm btn-success">Сохранить</button>
+                                          </div>
+                                        </div>
+                                      </a>
+                                    </h6>
+                                  </div>
+                                  <div id="collapseDashbord" class="collapse" role="tabpanel" aria-labelledby="headingDashbord" data-parent="#accordion">
+                                    <div class="card-body">
+
+                                    </div>
+                                  </div>
+                                </div>
+
+
+                                <div class="card">
+                                  <div class="card-header" role="tab" id="headingIntegry">
+                                    <h6 class="mb-0">
+                                      <a class="collapsed" data-toggle="collapse" href="#collapseIntegry" aria-expanded="false" aria-controls="collapseIntegry" style="color: #fff;">
+                                        <div class="row">
+                                          <div class="col-6 mt-2 pl-3" style="color: #000;">
+                                            Интеграция
+                                          </div>
+                                          <div class="col-6 text-right">
+                                            <button type="button" class="btn btn-sm btn-success">Сохранить</button>
+                                          </div>
+                                        </div>
+                                      </a>
+                                    </h6>
+                                  </div>
+                                  <div id="collapseIntegry" class="collapse" role="tabpanel" aria-labelledby="headingIntegry" data-parent="#accordion">
                                     <div class="card-body">
 
                                     </div>
