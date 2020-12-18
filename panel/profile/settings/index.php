1811d4e822034a11f5849e8ccb2332d459f7ac93
CoderButerbroder <dimos-eskimos2014@yandex.ru>
2020-12-18 13:29:40 +0300

    фывфыв

---
+++ panel/profile/settings/index.php
@@ -0,0 +1,25 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/assets/template/gen_header.php');?>
+<?php /*тут метатеги*/?>
+<title>Панель - FULLDATA ЛЕНПОЛИГРАФМАШ</title>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/assets/template/header_panel.php');?>
+
+<nav class="page-breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Настройки</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Настройки пользоватля</li>
+  </ol>
+</nav>
+
+<div class="row">
+    <div class="col-md-12 stretch-card">
+                  <div class="card">
+                      <div class="card-body">
+                          тут настройки
+
+                      </div>
+                  </div>
+    </div>
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/assets/template/footer_panel.php');?>
