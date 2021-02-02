39f4a06bd1f781c5189f4c5133e7103f07def059
Xokma <62008876+Xokma@users.noreply.github.com>
2021-02-02 15:28:56 +0300

    +test

---
+++ admin/cron/debug_entity_test.php
@@ -0,0 +1,32 @@
+<?php
+
+error_reporting(0);
+// ini_set('error_reporting', E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// session_start();
+// include('/home/httpd/vhosts/api.kt-segment.ru/httpdocs/general/core.php');
+// $settings = new Settings;
+// var_dump($database);
+
+
+// поиск компаний с ломаным инн 9 или 11 символов
+
+// $statement = $database->prepare("SELECT * FROM `MAIN_entity` ");
+// $statement->execute();
+// $data = $statement->fetchAll(PDO::FETCH_OBJ);
+//
+// foreach($data as $key => $value) {
+//   if(iconv_strlen($value->inn) == 9 || iconv_strlen($value->inn) == 11 || trim($value->data_fns) == ''){
+//     echo 'len: ';
+//     echo iconv_strlen($value->inn);
+//     echo ' inn: ';
+//     echo $value->inn;
+//     echo '</br>';
+//   }
+//
+// }
+
+
+
+?>
