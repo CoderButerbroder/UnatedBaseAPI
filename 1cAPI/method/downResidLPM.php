38d4a5a93046563d4ca5e61db6d51ffb16478b3b
Dmitriy Ivanov <dimos-eskimos2014@yandex.ru>
2021-02-20 15:45:24 +0300

    добавление функций для работы с 1с аренда готовы на 50%

--- 1cAPI/method/downResidLPM.php
+++ 1cAPI/method/downResidLPM.php
@@ -1,9 +1,6 @@
 <?
 /* Загрузка рещидентов из 1С-аренда
 
-$id_event_on_referer = id меропритяи на рефере
-$id_entity = id компании в единой базе данных
-$status = статус посещеения
 
 */
 include($_SERVER['DOCUMENT_ROOT'].'/v.1.0/settings.php');
@@ -13,31 +10,23 @@ if (!$_GET['key']) {echo json_encode(array('response' => false, 'description' =>
 require_once($_SERVER['DOCUMENT_ROOT'].'/general/core.php');
 $settings = new Settings;
 
-if (($_GET['key'] == $settings->get_global_settings('api_key_1c_rent')) && $_FILES) {
+if ($_GET['key'] == $settings->get_global_settings('api_key_1c_rent')) {
 
       // name поля = null
+      var_dump($_FILES);
 
-      $xml = simplexml_load_file("residents.xml");
+      $xml = simplexml_load_file($_FILES['residents']['tmp_name']);
       $json = json_encode($xml,JSON_UNESCAPED_UNICODE);
       $array = json_decode($json,TRUE);
 
       var_dump($json);
 
-      // для бэкапирования (если понадобиться)
+      $check = $settings->update_residents_lpm($json);
 
-      // $uploaddir = '/var/www/uploads/';
-      // $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-      //
-      //
-      // if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-      //     echo "Файл корректен и был успешно загружен.\n";
-      // } else {
-      //     echo "Возможная атака с помощью файловой загрузки!\n";
-      // }
-
+      echo $check;
 
 } else {
-      echo json_encode(array('response' => false, 'description' => 'Не все обязательные поля были заполнены для добавления посещения физ. лица мероприятия', 'data_referer' => $_POST),JSON_UNESCAPED_UNICODE);
+      echo json_encode(array('response' => false, 'description' => 'Не все обязательные поля были заполнены', 'data_referer' => $_POST, 'file' => $_FILES),JSON_UNESCAPED_UNICODE);
       exit;
 }
 
