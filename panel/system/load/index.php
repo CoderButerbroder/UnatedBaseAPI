f820b8b6da215fbc3f6d88362aa310b99b2adf41
CoderButerbroder <dimos-eskimos2014@yandex.ru>
2021-03-01 14:03:16 +0300

    добавление настроек системы

---
+++ panel/system/load/index.php
@@ -0,0 +1,21 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/assets/template/gen_header.php');?>
+<?php /*тут метатеги*/?>
+<title>Настройки нагрузка - FULLDATA ЛЕНПОЛИГРАФМАШ</title>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/assets/template/header_panel.php'); ?>
+
+<?php if (!$data_user_rules->sistem->rule->settings->value) {?>
+
+    <div class="container-fluid text-center">
+        <div class="alert alert-danger" role="alert"><i class="mb-3" style="width: 40px; height: 40px;" data-feather="alert-triangle"></i><h4>Доступ запрещен</h4><p>Доступ к данному разделу запрещен для вашей роли, запросите доступ у администратора</p> <a class="btn btn-primary m-4" href="/panel">Вернуться на главную</a></div>
+    </div>
+
+<?php } else { ?>
+
+
+
+
+<?php }  ?>
+
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/assets/template/footer_panel.php');?>
