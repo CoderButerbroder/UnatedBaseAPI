bf7ebb4ad97842771bc2e0ee0c931801727ed90a
Victor Kulik <vi9908@yandex.ru>
2021-03-05 15:55:06 +0300

    ewr

---
+++ panel/system/load/actions/get_data_chart.php
@@ -0,0 +1,306 @@
+<?php
+// ini_set('error_reporting', E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+session_start();
+
+if (!isset($_SESSION["key_user"])) {
+  echo json_encode(array('response' => false, 'description' => 'Ошибка проверки авторизации'), JSON_UNESCAPED_UNICODE);
+  exit();
+}
+
+$period_select = trim($_POST["period"]);
+
+if ($period_select != 'year' && $period_select != 'month' && $period_select != 'week' && $period_select != 'day' ) {
+  echo json_encode(array('response' => false, 'description' => 'Ошибка полученного параметра'), JSON_UNESCAPED_UNICODE);
+  exit();
+}
+
+$arr_select_month = array('1' => (object) array('name' => 'Январь', ),
+                          '2' => (object) array('name' => 'Февраль', ),
+                          '3' => (object) array('name' => 'Март', ),
+                          '4' => (object) array('name' => 'Апрель', ),
+                          '5' => (object) array('name' => 'Май', ),
+                          '6' => (object) array('name' => 'Июнь', ),
+                          '7' => (object) array('name' => 'Июль', ),
+                          '8' => (object) array('name' => 'Август', ),
+                          '9' => (object) array('name' => 'Сентябрь', ),
+                          '10' => (object) array('name' => 'Октябрь', ),
+                          '11' => (object) array('name' => 'Ноябрь', ),
+                          '12' => (object) array('name' => 'Декабрь' ) );
+
+$arr_data_ru_json_chart = json_decode(file_get_contents('https://'.$_SERVER["SERVER_NAME"].'/assets/vendors/apexcharts/ru.json'));
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/general/core.php');
+// session_destroy();
+session_write_close();
+
+$settings = new Settings;
+
+function add_null_in_data_week( $arr_in ) {
+  foreach( $arr_in as $key => $value ) {
+    if($value->weekd >= 1 && $value->weekd <= 9 ){
+      $value->weekd = '0'.$value->weekd;
+    }
+  }
+}
+
+function get_list_date_arr( $arr_in ) {
+  global $period_select;
+  global $arr_data_period;
+  foreach ($arr_in as $key => $value) {
+    if ( $period_select == 'day' )   $data_i = strtotime($value->yeard.'-'.$value->monthd.'-'.$value->dayd);
+    if ( $period_select == 'week' )  $data_i = strtotime(  $value->yeard.'W'.$value->weekd );
+    if ( $period_select == 'month')  $data_i = strtotime( '01.'.$value->monthd.'.'.$value->yeard );
+    if ( $period_select == 'year' )  $data_i = strtotime( '01.01.'.$value->yeard );
+    if (!in_array($data_i, $arr_data_period)) {
+        array_push($arr_data_period, $data_i);
+    }
+  }
+}
+
+function set_get_arr_result($name, $arr_in, $type){
+  global $period_select;
+  global $arr_data_period;
+
+  $arr_temp = (object) [];
+  $arr_temp->name = $name;
+  $arr_temp->data = [];
+  $arr_temp->type = $type;
+
+  foreach ($arr_data_period as $key_data => $value_data) {
+    $flag_value_bool = false;
+    $flag_value_int = 0;
+
+    foreach ($arr_in as $key2 => $value2) {
+      if($period_select == 'day'){
+        $date1 = strtotime($value2->yeard.'-'.$value2->monthd.'-'.$value2->dayd);
+      }
+      if($period_select == 'week'){
+        $date1 = strtotime($value2->yeard.'W'.$value2->weekd);
+      }
+      if($period_select == 'month'){
+        $date1 = strtotime('01.'.$value2->monthd.'.'.$value2->yeard);
+      }
+      if($period_select == 'year'){
+        $date1 = strtotime('01.01.'.$value2->yeard);
+      }
+
+      if ($value_data == $date1) {
+        $flag_value_bool = true;
+        $flag_value_int = $value2->sum;
+        break;
+      }
+    }
+    if ($flag_value_bool) {
+      array_push($arr_temp->data, $flag_value_int);
+    } else {
+      array_push($arr_temp->data, 0);
+    }
+  }
+
+  return $arr_temp;
+}
+
+
+if($_POST["chart"] == 'load_method') {
+
+    $arr_data_method = $settings->get_log_api_response_group_by_method();
+
+    $arr_result = (object) [];
+    $arr_result->data = [];
+    $arr_result->xaxis = [];
+
+    foreach($arr_data_method as $key => $value){
+      array_push($arr_result->data, $value->sum);
+      array_push($arr_result->xaxis, $value->method);
+    }
+
+    echo json_encode($arr_result, JSON_UNESCAPED_UNICODE);
+
+}
+
+if($_POST["chart"] == 'date_method') {
+
+  $arr_data_method_load = $settings->get_log_api_response_group_by(false, $period_select);
+
+  // $arr_data_users = $settings->get_count_main_users_groupby_time_reg(true, $period_select);
+
+  // $arr_begin_users = [];
+  $arr_data_period = [];
+
+  if ($period_select == 'week') {
+    add_null_in_data_week($arr_data_method_load);
+  }
+
+  if (is_array($arr_data_method_load) && count($arr_data_method_load) > 0 && $arr_data_method_load != 0 ) get_list_date_arr($arr_data_method_load);
+
+  sort($arr_data_period);
+
+  $arr_result = (object) [];
+  $arr_result->data = [];
+  $arr_result->time = [];
+
+
+  $arr_name_method = [];
+  foreach ($arr_data_method_load as $key => $value) {
+    array_push($arr_name_method, $value->method );
+  }
+
+  $arr_name_method = array_unique($arr_name_method);
+
+  foreach ($arr_name_method as $key_name_m => $value_name_m) {
+    $flag_break = false;
+    $arr_temp = (object) [];
+    $arr_temp->name = $value_name_m;
+    $arr_temp->data = [];
+    $arr_temp->type = 'line';
+
+    foreach ($arr_data_period as $key_time => $value_time) {
+      foreach ($arr_data_method_load as $key_method => $value_method) {
+        if($period_select == 'day'){
+         $temp_time_method = strtotime($value_method->yeard.'-'.$value_method->monthd.'-'.$value_method->dayd);
+       }
+       if($period_select == 'week'){
+         $temp_time_method = strtotime($value_method->yeard.'W'.$value_method->weekd);
+       }
+       if($period_select == 'month'){
+         $temp_time_method = strtotime('01.'.$value_method->monthd.'.'.$value_method->yeard);
+       }
+       if($period_select == 'year'){
+         $temp_time_method = strtotime('01.01.'.$value_method->yeard);
+       }
+        if ($value_name_m == $value_method->method && $temp_time_method == $value_time) {
+           array_push($arr_temp->data, $value_method->sum);
+           $flag_break = true;
+           break;
+        }
+      }
+      if (!$flag_break) {
+        array_push($arr_temp->data, 0);
+      }
+    }
+    array_push($arr_result->data,  $arr_temp );
+  }
+
+
+  foreach ($arr_data_period as $key => $value) {
+    if($period_select == 'day'){
+      $data_key = date('d', $value)." ".$arr_select_month[date('n', $value)]->name." ".date('Y', $value);
+
+    }
+    if($period_select == 'week'){
+      $data_key = intval(date('W', $value))." Нед. ".$arr_select_month[date('n', $value)]->name." ".date('Y', $value);
+
+    }
+    if($period_select == 'month'){
+      $data_key = $arr_select_month[date('n', $value)]->name." ".date('Y', $value);
+
+    }
+    if($period_select == 'year'){
+      $data_key = date('Y', $value);
+    }
+    array_push( $arr_result->time,  $data_key );
+  }
+
+  echo json_encode($arr_result, JSON_UNESCAPED_UNICODE);
+
+}
+
+if($_POST["chart"] == 'date_method_Scatter') {
+
+  $arr_data_method_load = $settings->get_log_api_response_group_by(false, $period_select);
+
+
+  $arr_data_period = [];
+
+  if ($period_select == 'week') {
+    add_null_in_data_week($arr_data_method_load);
+  }
+
+  if (is_array($arr_data_method_load) && count($arr_data_method_load) > 0 && $arr_data_method_load != 0 ) get_list_date_arr($arr_data_method_load);
+
+  sort($arr_data_period);
+
+  $arr_result = (object) [];
+  $arr_result->data = [];
+  $arr_result->time = [];
+  $arr_result->colors = [];
+
+
+
+  $arr_name_method = [];
+  foreach ($arr_data_method_load as $key => $value) {
+    array_push($arr_name_method, $value->method );
+  }
+
+  $arr_name_method = array_unique($arr_name_method);
+
+  foreach ($arr_name_method as $key => $value) {
+    array_push( $arr_result->colors,  '#' . dechex(rand(0,10000000)) );
+  }
+
+  foreach ($arr_name_method as $key_name_m => $value_name_m) {
+    $flag_break = false;
+    $arr_temp = (object) [];
+    $arr_temp->name = $value_name_m;
+    $arr_temp->data = [];
+    // $arr_temp->type = 'line';
+
+    foreach ($arr_data_period as $key_time => $value_time) {
+      foreach ($arr_data_method_load as $key_method => $value_method) {
+        if($period_select == 'day'){
+         $temp_time_method = strtotime($value_method->yeard.'-'.$value_method->monthd.'-'.$value_method->dayd);
+       }
+       if($period_select == 'week'){
+         $temp_time_method = strtotime($value_method->yeard.'W'.$value_method->weekd);
+       }
+       if($period_select == 'month'){
+         $temp_time_method = strtotime('01.'.$value_method->monthd.'.'.$value_method->yeard);
+       }
+       if($period_select == 'year'){
+         $temp_time_method = strtotime('01.01.'.$value_method->yeard);
+       }
+        if ($value_name_m == $value_method->method && $temp_time_method == $value_time) {
+          array_push($arr_temp->data, $value_method->sum );
+           // array_push($arr_temp->data, [date('m.Y', $value_time), $value_method->sum] );
+           $flag_break = true;
+           break;
+        }
+      }
+      if (!$flag_break) {
+        array_push($arr_temp->data, 0);
+        // array_push($arr_temp->data, [date('m.Y', $value_time), 0]);
+      }
+    }
+    array_push($arr_result->data,  $arr_temp );
+  }
+
+
+  foreach ($arr_data_period as $key => $value) {
+    if($period_select == 'day'){
+      $data_key = date('d', $value)." ".$arr_select_month[date('n', $value)]->name." ".date('Y', $value);
+
+    }
+    if($period_select == 'week'){
+      $data_key = intval(date('W', $value))." Нед. ".$arr_select_month[date('n', $value)]->name." ".date('Y', $value);
+
+    }
+    if($period_select == 'month'){
+      $data_key = $arr_select_month[date('n', $value)]->name." ".date('Y', $value);
+
+    }
+    if($period_select == 'year'){
+      $data_key = date('Y', $value);
+    }
+    array_push( $arr_result->time,  $data_key );
+  }
+
+
+
+
+  echo json_encode($arr_result, JSON_UNESCAPED_UNICODE);
+
+}
+
+?>
--- panel/system/load/index.php
+++ panel/system/load/index.php
@@ -19,23 +19,84 @@
     </ol>
   </nav>
 
-  <div class="row">
-      <div class="col-xl-12 stretch-card">
-        <div class="col-xl-6 grid-margin stretch-card">
-          <div class="card">
-            <div class="card-body" style="min-height: 420px; max-height: 420px;">
-              <h6 class="card-title">Общая нагрузка по методам (apex radar)</h6>
-              <div id="apexBar"></div>
-              <div id="spinner_apexBar" class="spinner-border text-primary" style="position: absolute; margin: -25px 0 0 -25px; top: 50%; left: 50%;  width: 3rem; height: 3rem; z-index:99999;" role="status">
+  <div class="row mt-3 mb-3">
+    <div class="col-md-12 stretch-card">
+        <div class="card">
+          <div class="card-header">
+             <div class="row align-items-start mb-2 my-auto">
+              <div class="col-md-7 my-auto">
+                <h5 class="card-title my-auto">НАГРУЗКА ПО МЕТОДАМ по Времени</h5>
+              </div>
+              <div class="col-md-5 d-flex justify-content-md-end" style="display:none;">
+                <div class="btn-group mb-3 mb-md-0" name_chart="date_method" role="group" aria-label="Basic example" id="btn_period_chart_line_date_method" >
+                  <button type="button" class="btn_period_chart btn btn-outline-primary" value="day">День</button>
+                  <button type="button" class="btn_period_chart btn btn-outline-primary" value="week">Неделя</button>
+                  <button type="button" class="btn_period_chart btn btn-primary" value="month" active>Месяц</button>
+                  <button type="button" class="btn_period_chart btn btn-outline-primary" value="year">Год</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card-body" style="min-height: 420px; /*max-height: 420;*/">
+            <div class="" id="div_chart_line_date_method" >
+              <div id="spinner_chart_line_date_method" class="spinner-border text-primary" style="position: absolute; margin: -25px 0 0 -25px; top: 50%; left: 50%;  width: 3rem; height: 3rem; z-index:99999;" role="status">
                 <span class="sr-only">Loading...</span>
               </div>
             </div>
           </div>
         </div>
-            <!-- <?php // json_encode($settings->get_log_api_response_group_by(false,'year')); ?> -->
+    </div>
+  </div>
 
-            <?php //echo json_encode($settings->get_log_api_response_group_by_method());?>
-      </div>
+  <div class="row mt-3 mb-3">
+    <div class="col-md-12 stretch-card">
+        <div class="card">
+          <div class="card-header">
+             <div class="row align-items-start mb-2 my-auto">
+              <div class="col-md-7 my-auto">
+                <h5 class="card-title my-auto">НАГРУЗКА ПО МЕТОДАМ по Времени</h5>
+              </div>
+              <div class="col-md-5 d-flex justify-content-md-end" style="display:none;">
+                <div class="btn-group mb-3 mb-md-0" name_chart="date_method_Scatter" role="group" aria-label="Basic example" id="btn_period_chart_line_date_method_Scatter" >
+                  <button type="button" class="btn_period_chart btn btn-outline-primary" value="day">День</button>
+                  <button type="button" class="btn_period_chart btn btn-outline-primary" value="week">Неделя</button>
+                  <button type="button" class="btn_period_chart btn btn-primary" value="month" active>Месяц</button>
+                  <button type="button" class="btn_period_chart btn btn-outline-primary" value="year">Год</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card-body" style="min-height: 420px; /*max-height: 420;*/">
+            <div class="" id="div_chart_line_date_method_Scatter" >
+              <div id="spinner_chart_line_date_method_Scatter" class="spinner-border text-primary" style="position: absolute; margin: -25px 0 0 -25px; top: 50%; left: 50%;  width: 3rem; height: 3rem; z-index:99999;" role="status">
+                <span class="sr-only">Loading...</span>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+
+  <div class="row">
+
+    <div class="col-md-6 stretch-card">
+        <div class="card">
+          <div class="card-header">
+             <div class="row align-items-start mb-2 my-auto">
+              <div class="col-md-7 my-auto">
+                <h5 class="card-title my-auto">ОБЩАЯ НАГРУЗКА ПО МЕТОДАМ</h5>
+              </div>
+            </div>
+          </div>
+          <div class="card-body" style="min-height: 420px; max-height: 420px;">
+            <div class="" id="div_chart_line_load_method" >
+              <div id="spinner_chart_line_load_method" class="spinner-border text-primary" style="position: absolute; margin: -25px 0 0 -25px; top: 50%; left: 50%;  width: 3rem; height: 3rem; z-index:99999;" role="status">
+                <span class="sr-only">Loading...</span>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
   </div>
 
   <script type="text/javascript">
@@ -46,7 +107,9 @@
     }],
     chart: {
       type: 'bar',
-      height: 350
+      height: 350,
+      defaultLocale: 'ru',
+      locales: [],
     },
     plotOptions: {
       bar: {
@@ -61,36 +124,174 @@
     }
   };
 
+  var options_line_ur = {
+    chart: {
+        height: 350,
+        type: "line",
+        stacked: false,
+        locales: [],
+        defaultLocale: 'ru',
+      },
+      dataLabels: {
+        enabled: false
+      },
+      series: [],
+      stroke: {
+        width: [4, 4]
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: "20%"
+        }
+      },
+      xaxis: {
+        categories: []
+      },
+      legend: {
+        horizontalAlign: "left",
+        offsetX: 40
+      }
+  };
+
+  var options_scatter = {
+    series: [ ],
+      chart: {
+      height: 350,
+      type: 'scatter',
+      zoom: {
+        type: 'xy'
+      },
+      locales: [],
+      defaultLocale: 'ru',
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    grid: {
+      xaxis: {
+        lines: {
+          show: true
+        }
+      },
+      yaxis: {
+        lines: {
+          show: true
+        }
+      },
+    },
+    // markers:{
+    //   colors: ['#F44336', '#E91E63', '#9C27B0']
+    // },
+    xaxis: {
+      categories: [],
+      // tickAmount: 10,
+      // type: 'datetime',
+    },
+  };
+
   $(document).ready(function() {
     $.getJSON('https://<?php echo $_SERVER["SERVER_NAME"]; ?>/assets/vendors/apexcharts/ru.json', function(data) {
-      <?php
-        $arr_data_method = $settings->get_log_api_response_group_by_method();
 
-        $arr_result = (object) [];
-        $arr_result->data = [];
-        $arr_result->xaxis = [];
+      var arr_chart = ['load_method', 'date_method', 'date_method_Scatter'];
 
-        foreach($arr_data_method as $key => $value){
-          array_push($arr_result->data, $value->sum);
-          array_push($arr_result->xaxis, $value->method);
+      arr_chart.forEach((element) => {
+        var btn_act = $('#btn_period_chart_line_'+element+' [active]')[0];
+        var btn_value = (btn_act != undefined) ? $(btn_act).val() : "month";
+        activate_charts(element, btn_value, data);
+      });
+
+      $('.btn_period_chart').on('click', function() {
+        if ($(this).hasClass('btn-primary')) {
+          return 0;
         }
+        var div_list_btn = $(this).parent();
+        var btn_child = $(div_list_btn).children();
+        var name_chart = $(div_list_btn).attr('name_chart');
+        // console.log(name_chart);
 
-        echo "var data_method_str = '".json_encode($arr_result, JSON_UNESCAPED_UNICODE)."';";
-      ?>
-      if (IsJsonString(data_method_str)){
-        var data_method = JSON.parse(data_method_str);
-        opt_chart_load["series"][0]["data"] = Object.values(data_method["data"]);
-        opt_chart_load["xaxis"]["categories"] = Object.values(data_method["xaxis"]);
-        opt_chart_load["locales"] = [data];
-        var chart = new ApexCharts(document.querySelector('#apexBar'), opt_chart_load);
-        chart.render();
-        $('#spinner_apexBar').hide('fast');
-      } else {
-        alerts('warning', 'Ошибка', 'Попробуйте позже');
-      }
+        $(btn_child).each(function(elem) {
+          var temp_btn = $(btn_child)[elem];
+          // console.log(temp_btn);
+          $(temp_btn).removeClass('btn-primary');
+          if(!$(temp_btn).hasClass('btn-outline-primary')) $(temp_btn).addClass('btn-outline-primary');
+          $(temp_btn).removeAttr('active');
+        });
+        if(!$(this).hasClass('btn-primary')) $(this).addClass('btn-primary');
+        $(this).removeClass('btn-outline-primary');
+        $(this).attr('active', 'active');
+
+        var child_body_card = $('#div_chart_line_'+name_chart).children();
+        $(child_body_card).each(block => {
+          var temp_div = $(child_body_card)[block];
+          if (!$(temp_div).hasClass('spinner-border')) {
+            $(temp_div).remove();
+          };
+        })
+
+        activate_charts(name_chart, $(this).val(), data);
+      });
+
+      // if (IsJsonString(data_method_str)){
+      //   var data_method = JSON.parse(data_method_str);
+      //   opt_chart_load["series"][0]["data"] = Object.values(data_method["data"]);
+      //   opt_chart_load["xaxis"]["categories"] = Object.values(data_method["xaxis"]);
+      //   opt_chart_load["locales"] = [data];
+      //   var chart = new ApexCharts(document.querySelector('#apexBar'), opt_chart_load);
+      //   chart.render();
+      //   $('#spinner_apexBar').hide('fast');
+      // } else {
+      //   alerts('warning', 'Ошибка', 'Попробуйте позже');
+      // }
 
     });
   });
+
+
+  function activate_charts(element, period, ru_local) {
+    $('#spinner_chart_line_'+ element).show("fast");
+    $('#btn_period_chart_line_'+ element).hide("fast");
+    $.ajax({
+      async: true,
+      cache: false,
+      type: 'POST',
+      url: 'https://<?php echo $_SERVER["SERVER_NAME"]; ?>/panel/system/load/actions/get_data_chart',
+      data: { "chart" : element, "period" : period },
+      success: function(result, status, xhr) {
+        if (IsJsonString(result)) {
+          ar_data = JSON.parse(result);
+          if(ar_data["response"] == false) {
+            alerts('warning', ar_data["description"], 'Попробуйте позже');
+          } else {
+          $('#spinner_chart_line_'+ element).hide("fast");
+          if (element == 'load_method') {
+            var options_chart = opt_chart_load;
+              options_chart["series"][0]["data"] = Object.values(ar_data["data"]);
+              options_chart["xaxis"]["categories"] = Object.values(ar_data["xaxis"]);
+          }
+          if (element == 'date_method') {
+            var options_chart = options_line_ur;
+            options_chart["series"] = Object.values(ar_data["data"]);
+            options_chart["xaxis"]["categories"] = Object.values(ar_data["time"]);
+          }
+          if (element == 'date_method_Scatter') {
+            var options_chart = options_scatter;
+            options_chart["series"] = Object.values(ar_data["data"]);
+            options_chart["xaxis"]["categories"] = Object.values(ar_data["time"]);
+            // options_chart["dataLabels"]["style"]["colors"] = Object.values(ar_data["colors"]);
+          }
+
+          options_chart["chart"]["locales"] = [ru_local];
+          var chart = new ApexCharts(document.querySelector('#div_chart_line_'+element), options_chart);
+          chart.render();
+          $('#btn_period_chart_line_'+ element).show("fast");
+          }
+        }
+      },
+      error: function(jqXHR, textStatus) {
+        alerts('error', 'Ошибка подключения', 'Попробуйте позже');
+      }
+    });
+  }
 
 
   </script>
